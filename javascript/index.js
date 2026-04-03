7e30df55d533f40beaf545d1a38cd891a9255928
Gaurav <khatrigarima073@gmail.com>
2026-04-03 18:17:25 +0530

    all frontend files by mohit sir

---
+++ javascript/index.js
@@ -0,0 +1,87 @@
+// let my ="mohit sharma";
+// console.log(my.charAt(4))
+// console.log(my.charCodeAt(6))
+// console.log(my.replace("m","r"))
+// console.log(my.slice(4))
+// console.log(my.slice(-5))
+// console.log(my.toLowerCase())
+// console.log(my.toUpperCase())
+// let x="      mohit   ";
+// let y="sharma";
+// console.log(x.concat(y))
+// console.log(x.trim())
+// let x="1,2,3,4,5,6,7,8,9";
+// console.log(x.split(","))
+// let a=[10, 20, 30, 40, 50, 60,100,900];
+// a.push(70);
+// a.push("garima")
+// a.push("khatri")
+// a.pop()
+// a.pop()
+// a.unshift("ajay")
+// a.unshift("yogesh")
+// a.shift()
+// a.shift()
+// let first=a.shift()
+// let last=a.pop()
+// a.push(first)
+// a.unshift(last)
+// console.log(a)
+// a.splice(3(index),2(remove element),"garima")
+
+// let a=[10, 20, 30, 40, 50, 60,100,900];
+// a.splice(5,3)
+// a.splice(0,5)
+// a.splice(1,0,70,80,90)
+// console.log(a)
+
+// let a=[10,20,30,40,50,60];
+
+// let b=a.map((item)=>{
+//     return item*2;
+// })
+
+// console.log(b)
+
+// function output(){
+//     console.log("hello")
+// }
+// output();
+
+// (function(){
+//     console.log("garima")
+// })();
+
+// (()=>{
+//     console.log("khatri")
+// })()
+
+// let a=10
+// a=20
+// console.log(a)
+
+// const a=20
+// console.log(a)
+
+// let a=10;
+// let b="10"
+// if(a===b){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+
+// const points= 10;
+// console.log(points);
+
+// const marks= new Number(300);
+// console.log(marks);
+
+// console.log(marks.toString().length);
+
+// console.log(typeof(marks));
+
+// console.log(marks.toFixed(1));
+
